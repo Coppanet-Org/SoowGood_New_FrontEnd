ff8852e439cedcbd20a9f782406978d0782a0b67
ShaikatCopnet <shaikat.b@coppanet.com>
2023-08-15 21:21:28 +0600

    ....

--- src/app/proxy/services/doctor-schedule.service.ts
+++ src/app/proxy/services/doctor-schedule.service.ts
@@ -1,6 +1,6 @@
 import { RestService, Rest } from '@abp/ng.core';
 import { Injectable } from '@angular/core';
-import type { DoctorScheduleDaySessionInputDto, DoctorScheduleDto, DoctorScheduleInputDto, ResponseDto } from '../dto-models/models';
+import type { DoctorScheduleDto, DoctorScheduleInputDto, ResponseDto } from '../dto-models/models';
 
 @Injectable({
   providedIn: 'root',
@@ -14,15 +14,6 @@ export class DoctorScheduleService {
       method: 'POST',
       url: '/api/app/doctor-schedule',
       body: input,
-    },
-    { apiName: this.apiName,...config });
-  
-
-  createSession = (inputDto: DoctorScheduleDaySessionInputDto, config?: Partial<Rest.Config>) =>
-    this.restService.request<any, ResponseDto>({
-      method: 'POST',
-      url: '/api/app/doctor-schedule/session',
-      body: inputDto,
     },
     { apiName: this.apiName,...config });
   
@@ -59,20 +50,19 @@ export class DoctorScheduleService {
     { apiName: this.apiName,...config });
   
 
+  getScheduleListByDoctorId = (doctorId: number, config?: Partial<Rest.Config>) =>
+    this.restService.request<any, DoctorScheduleDto[]>({
+      method: 'GET',
+      url: `/api/app/doctor-schedule/schedule-list-by-doctor-id/${doctorId}`,
+    },
+    { apiName: this.apiName,...config });
+  
+
   update = (input: DoctorScheduleInputDto, config?: Partial<Rest.Config>) =>
     this.restService.request<any, ResponseDto>({
       method: 'PUT',
       url: '/api/app/doctor-schedule',
       body: input,
-    },
-    { apiName: this.apiName,...config });
-  
-
-  updateSession = (inputDto: DoctorScheduleDaySessionInputDto, config?: Partial<Rest.Config>) =>
-    this.restService.request<any, ResponseDto>({
-      method: 'PUT',
-      url: '/api/app/doctor-schedule/session',
-      body: inputDto,
     },
     { apiName: this.apiName,...config });
 
