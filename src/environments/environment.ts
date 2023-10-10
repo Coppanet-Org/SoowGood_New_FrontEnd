76bd45a7fecfbef18ee0df3251c2549fd00921ee
ShaikatCopnet <shaikat.b@coppanet.com>
2023-10-10 17:05:18 +0600

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ import { Environment } from '@abp/ng.core';
 
 const baseUrl = 'https://localhost:4202';
 const apiUrl = 'https://localhost:44339';
-//const issuerUrl = 'https://198.38.92.117';
+const issuerUrl = 'https://198.38.92.117';
 
-const issuerUrl = 'https://localhost:44339';
+//const issuerUrl = 'https://localhost:44339';
 
 //const localfileUrl = 'https://devsoowgoodnew.azurewebsites.net/';
 //const PaymentUrl = 'https://payment.soowgood.com/';
