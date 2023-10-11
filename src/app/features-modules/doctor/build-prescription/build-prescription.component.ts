1be187916fcc3ec39033f3b40fba79f540bd5390
muntasir <muntasirudoy4@gmaiul.com>
2023-10-11 12:35:11 +0600

    Prescription Services

--- src/app/features-modules/doctor/build-prescription/build-prescription.component.ts
+++ src/app/features-modules/doctor/build-prescription/build-prescription.component.ts
@@ -1,13 +1,24 @@
 import { TosterService } from 'src/app/shared/services/toster.service';
 import { AppointmentService } from 'src/app/proxy/services';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppointmentDto } from 'src/app/proxy/dto-models';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { symptoms } from 'src/app/shared/utils/patient-symptoms';
 
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-build-prescription',
@@ -16,14 +27,14 @@ import { symptoms } from 'src/app/shared/utils/patient-symptoms';
 })
 export class BuildPrescriptionComponent implements OnInit {
   form!: FormGroup;
-  prescriptionForm!: FormGroup
+  prescriptionForm!: FormGroup;
   appointmentInfo: AppointmentDto = {};
   specialCaseChecked: boolean = false;
   options: any[] = symptoms;
   filteredOptions!: Observable<string[]>;
 
   symptomsControls: FormControl[] = [];
-  today!:any
+  today!: any;
 
   selectedSymptoms: string[] = [];
   durationList = [
@@ -50,15 +61,14 @@ export class BuildPrescriptionComponent implements OnInit {
     private route: ActivatedRoute,
     private Router: Router,
     private AppointmentService: AppointmentService,
-    private TosterService: TosterService
+    private TosterService: TosterService,
+
   ) {}
 
   ngOnInit(): void {
-  
-this.today = new Date().toDateString()
-this.todayTime = new Date().toLocaleTimeString()
+    this.today = new Date().toDateString();
+    this.todayTime = new Date().toLocaleTimeString();
 
-    
     const { aptId } = this.route.snapshot.params;
     if (aptId) {
       this.AppointmentService.get(aptId).subscribe((res) => {
@@ -74,55 +84,41 @@ this.todayTime = new Date().toLocaleTimeString()
     }
 
     this.loadForm();
-
   }
 
-
-
-  
   displayFn(symptom: string): string {
     return symptom || '';
   }
-  
 
   private _filter(value: any): string[] {
     const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
   }
-
-
 
   loadForm() {
     this.form = this.fb.group({
       patientName: ['Hasibn'],
-      height:[''],
-      weight:[''],
-      date:[""],
-      time:[''],
-      age:['']
+      height: [''],
+      weight: [''],
+      date: [''],
+      time: [''],
+      age: [''],
       // Add other patient-related fields as needed (age, weight, date, time, etc.)
     });
 
     this.prescriptionForm = this.fb.group({
-      followUp:'',
-      advice:'',
-      chiefComplaints: this.fb.array([
-        this.createChiefComplaintFormGroup()
-      ]),
-      findings: this.fb.array([
-        this.createFindingsFormGroup()
-      ]),
-      medicineSchedule: this.fb.array([
-        this.createMedicineScheduleFormGroup()
-      ]),
-      diagnosis: this.fb.array([
-        this.createMedicineScheduleFormGroup()
-      ]),
+      followUp: '',
+      advice: '',
+      chiefComplaints: this.fb.array([this.createChiefComplaintFormGroup()]),
+      findings: this.fb.array([this.createFindingsFormGroup()]),
+      medicineSchedule: this.fb.array([this.createMedicineScheduleFormGroup()]),
+      diagnosis: this.fb.array([this.createMedicineScheduleFormGroup()]),
     });
   }
 
-
-// get each array
+  // get each array
   get chiefComplaints() {
     return this.prescriptionForm.get('chiefComplaints') as FormArray;
   }
@@ -138,42 +134,39 @@ this.todayTime = new Date().toLocaleTimeString()
   //each form group
   createChiefComplaintFormGroup() {
     return this.fb.group({
-      symptoms: ['',Validators.required],
-      durationDay: ['1',Validators.required],
-      durationTime: ['day',Validators.required],
+      symptoms: ['', Validators.required],
+      durationDay: ['1', Validators.required],
+      durationTime: ['day', Validators.required],
       condition: [''],
-      problems: ['',Validators.required],
-      comments: ['',Validators.required],
+      problems: ['', Validators.required],
+      comments: [''],
     });
   }
   createFindingsFormGroup() {
     return this.fb.group({
-      observations: [''],
-      problems: [''],
-      comments: ['']
+      observations: ['', Validators.required],
+      problems: ['', Validators.required],
+      comments: [''],
     });
-   
   }
   createMedicineScheduleFormGroup() {
     return this.fb.group({
-      specialCaseChecked:[false],
-      medicineName: [''],
-      durationDay: ['1'],
-      durationTime: ['day'],
-      timing: [''],
+      specialCaseChecked: [false],
+      medicineName: ['', Validators.required],
+      durationDay: ['1', Validators.required],
+      durationTime: ['day', Validators.required],
+      timing: ['', Validators.required],
       timingDay: [''],
       comments: [''],
     });
-   
   }
   createDiagnosisFormGroup() {
     return this.fb.group({
-      testName: [''],
+      testName: ['', Validators.required],
       comments: [''],
     });
-   
   }
-// add new row
+  // add new row
   addChiefComplaint() {
     this.chiefComplaints.push(this.createChiefComplaintFormGroup());
   }
@@ -200,11 +193,141 @@ this.todayTime = new Date().toLocaleTimeString()
     this.diagnosis.removeAt(index);
   }
 
+  submitPrescription() {
+    const formattedMedicineSchedule =
+      this.prescriptionForm.value.medicineSchedule.map((medicine: any) => ({
+        ...medicine,
+        timingDay: Array.isArray(medicine.timingDay)
+          ? medicine.timingDay.join(', ')
+          : medicine.timingDay,
+      }));
 
-  submitPrescription(){
-    const formattedMedicineSchedule = this.prescriptionForm.value.medicineSchedule.map((medicine:any) => ({
-      ...medicine,
-      timingDay: Array.isArray(medicine.timingDay) ? medicine.timingDay.join(', ') : medicine.timingDay,
-    }));
+console.log();
+
+
+
   }
+
+  // generatePDF(action = 'open') {
+  //   const docDefinition = {
+  //     content: [
+  //       {
+  //         text: `Dr. doctorName}`,
+  //         style: 'header',
+  //       },
+  //       {
+  //         text: 'General Medicine',
+  //         style: 'subheader',
+  //       },
+  //       {
+  //         text: 'Dhaka Medical College & Hospital, Dhaka',
+  //         style: 'subheader',
+  //       },
+  //       {
+  //         text: `BMDC No. : 03210225423`,
+  //         style: 'subheader',
+  //       },
+  //       {
+  //         columns: [
+  //           {
+  //             text: `Patient Name: patientName}`,
+  //             width: 150,
+  //           },
+  //           {
+  //             text: `Age: age}`,
+  //             width: 50,
+  //           },
+  //           {
+  //             text: `Weight: weight} kg`,
+  //             width: 100,
+  //           },
+  //           {
+  //             text: `Date: ${new Date().toLocaleDateString()}`,
+  //             width: 120,
+  //           },
+  //           {
+  //             text: `Time: ${new Date().toLocaleTimeString()}`,
+  //             width: 120,
+  //           },
+  //         ],
+  //         margin: [0, 15, 0, 10],
+  //       },
+  //       {
+  //         table: {
+  //           widths: [150, 350],
+  //           border:[0,0,0,0],
+  //           body: [ [
+  //             {
+  //               border: [0, 0, 0, 0], // Set all border values to 0 to make them transparent
+  //               stack: [
+  //                 {
+  //                   text: 'Header 1',
+  //                   style: 'listHeader',
+  //                 },
+  //                 {
+  //                   ul: [
+  //                     {
+  //                       text: 'Styled Item 1',
+  //                       style: 'listItem',
+  //                       display: 'inline',
+  //                     },
+  //                     {
+  //                       text: 'Styled Item 2',
+  //                       style: 'listItem',
+  //                       display: 'inline',
+  //                     },
+  //                   ],
+  //                 },
+  //                 {
+  //                   text: 'Header 1',
+  //                   style: 'listHeader',
+  //                   margin:[15,0,15,0]
+  //                 },
+  //                 {
+  //                   ul: [
+  //                     {
+  //                       text: 'Styled Item 1',
+  //                       style: 'listItem',
+  //                       display: 'inline',
+  //                     },
+  //                     {
+  //                       text: 'Styled Item 2',
+  //                       style: 'listItem',
+  //                       display: 'inline',
+  //                     },
+  //                   ],
+  //                 },
+  //               ],
+  //             },
+  //             '',
+  //           ],
+  //           ],
+  //         },
+  //       },
+  //       // Add sections for Chief Complaints, Findings, Medication & Schedule, Diagnosis, Follow Up, and Advice here
+  //     ],
+  //     styles: {
+  //       header: {
+  //         fontSize: 22,
+  //         bold: true,
+  //         margin: [0, 0, 0, 10], // [left, top, right, bottom]
+  //       },
+  //       subheader: {
+  //         fontSize: 12,
+  //         margin: [0, 0, 0, 5],
+  //       },
+  //       sectionHeader: {
+  //         fontSize: 12,
+  //         margin: [0, 15, 0, 10],
+  //       },
+  //       listItem: {
+  //         margin: [5, 0],
+          
+  //       },
+  //     },
+  //   };
+    
+  //   pdfMake.createPdf(docDefinition as any).open({}, window);
+    
+  // }
 }
