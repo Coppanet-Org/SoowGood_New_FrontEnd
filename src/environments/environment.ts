5c20a5b0455fb01eb7b1788b315fd9e33e5083be
ShaikatCopnet <shaikat.b@coppanet.com>
2023-10-24 13:17:07 -0700

    ......

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Environment } from '@abp/ng.core';
 
 const baseUrl = 'https://localhost:4202';
 const apiUrl = 'https://localhost:44339';//'https://198.38.92.117:4437'; 
-const issuerUrl = 'https://localhost:44380' ;// 'https://198.38.92.117';
+const issuerUrl = 'https://localhost:44380';// 'https://198.38.92.117';
 
 //const localfileUrl = 'https://devsoowgoodnew.azurewebsites.net/';
 //'https://localhost:44339';'https://localhost:44339';//
