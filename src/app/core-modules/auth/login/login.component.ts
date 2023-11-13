0ec36ee392e0ea282b81939ba0455d036a4d8b5d
ShaikatCopnet <shaikat.b@coppanet.com>
2023-11-13 13:03:20 +0600

    Loging Component Updated

--- src/app/core-modules/auth/login/login.component.ts
+++ src/app/core-modules/auth/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit, OnDestroy {
     mobileNo: '',
     password: '',
   };
-  formSubmitted:boolean = false
+  formSubmitted: boolean = false
   errorMessage: string = '';
   loginForm!: FormGroup;
   loginDto: LoginDto = {} as LoginDto;
@@ -252,74 +252,63 @@ export class LoginComponent implements OnInit, OnDestroy {
               var un = username.split('@')[0];
               this.authService
                 .loginByUserDto(this.loginDto).subscribe((loginResponse: LoginResponseDto) => {
-                  if (loginResponse.roleName[0] == "Doctor") {
-                    if (loginResponse.success) {
-                      this.isLoading = false;
-                      this.subs.sink = this.doctorProfileService.getByUserName(loginResponse.userName ? loginResponse.userName : "")
-                        .subscribe((doctorDto: DoctorProfileDto) => {
-                          let saveLocalStorage = {
-                            identityNumber: doctorDto.identityNumber,
-                            doctorName: doctorDto.fullName,
-                            bmdcRegNo: doctorDto.bmdcRegNo,
-                            isActive: doctorDto.isActive,
-                            userId: doctorDto.userId,
-                            id: doctorDto.id,
-                            specialityId: doctorDto.specialityId,
-                            profileStep: doctorDto.profileStep,
-                            createFrom: doctorDto.createFrom,
-                            userType: loginResponse.roleName.toString().toLowerCase()
-                          }
-                          this.NormalAuth.setAuthInfoInLocalStorage(saveLocalStorage)
-                          if (doctorDto.profileStep == 1 || doctorDto.profileStep == 2) {
-                            userType = '/signup';
-                          }
-                          else {
-                            userType = (doctorDto.isActive ? (loginResponse.roleName.toString() + '/dashboard') : (loginResponse.roleName.toString() + '/profile-settings/basic-info'));
-                          }
-                          this._router.navigate([userType.toLowerCase()], {
-                            state: { data: doctorDto } // Pass the 'res' object as 'data' in the state object
-                          }).then(r => {
-                            this.ToasterService.customToast(loginResponse.message ? loginResponse.message : " ", 'success')
-                          });
+                  if (loginResponse.success && loginResponse.roleName[0] == "Doctor") {
+                    this.isLoading = false;
+                    this.subs.sink = this.doctorProfileService.getByUserName(loginResponse.userName ? loginResponse.userName : "")
+                      .subscribe((doctorDto: DoctorProfileDto) => {
+                        let saveLocalStorage = {
+                          identityNumber: doctorDto.identityNumber,
+                          doctorName: doctorDto.fullName,
+                          bmdcRegNo: doctorDto.bmdcRegNo,
+                          isActive: doctorDto.isActive,
+                          userId: doctorDto.userId,
+                          id: doctorDto.id,
+                          specialityId: doctorDto.specialityId,
+                          profileStep: doctorDto.profileStep,
+                          createFrom: doctorDto.createFrom,
+                          userType: loginResponse.roleName.toString().toLowerCase()
+                        }
+                        this.NormalAuth.setAuthInfoInLocalStorage(saveLocalStorage)
+                        if (doctorDto.profileStep == 1 || doctorDto.profileStep == 2) {
+                          userType = '/signup';
+                        }
+                        else {
+                          userType = (doctorDto.isActive ? (loginResponse.roleName.toString() + '/dashboard') : (loginResponse.roleName.toString() + '/profile-settings/basic-info'));
+                        }
+                        this._router.navigate([userType.toLowerCase()], {
+                          state: { data: doctorDto } // Pass the 'res' object as 'data' in the state object
+                        }).then(r => {
+                          this.ToasterService.customToast(loginResponse.message ? loginResponse.message : " ", 'success')
                         });
-                    }
-                    else {
-                      this.isLoading = false;
-                      this.hasError = true;
-                      this.ToasterService.customToast(loginResponse.message ? loginResponse.message : " ", 'error');
-                    }
+                      });
+
+
                   }
 
-                  else if (loginResponse.roleName[0] == "Patient") {
-                    if (loginResponse.success) {
-                      this.isLoading = false;
-                      this.subs.sink = this.PatientProfileService.getByUserName(loginResponse.userName ? loginResponse.userName : "")
-                        .subscribe((patientDto: PatientProfileDto) => {
-                          console.log(patientDto);
-                          let saveLocalStorage = {
-                            userId: patientDto.userId,
-                            id: patientDto.id,
-                            userType: loginResponse.roleName.toString().toLowerCase()
-                          }
-                          this.NormalAuth.setAuthInfoInLocalStorage(saveLocalStorage)
-                          let userType = (loginResponse.roleName.toString() + '/dashboard')
+                  else if (loginResponse.success && loginResponse.roleName[0] == "Patient") {
+                    this.isLoading = false;
+                    this.subs.sink = this.PatientProfileService.getByUserName(loginResponse.userName ? loginResponse.userName : "")
+                      .subscribe((patientDto: PatientProfileDto) => {
+                        console.log(patientDto);
+                        let saveLocalStorage = {
+                          userId: patientDto.userId,
+                          id: patientDto.id,
+                          userType: loginResponse.roleName.toString().toLowerCase()
+                        }
+                        this.NormalAuth.setAuthInfoInLocalStorage(saveLocalStorage)
+                        let userType = (loginResponse.roleName.toString() + '/dashboard')
 
-                          this._router.navigate([userType.toLowerCase()], {
-                            state: { data: patientDto } // Pass the 'res' object as 'data' in the state object
-                          }).then(r => {
-                            this.ToasterService.customToast(loginResponse.message ? loginResponse.message : " ", 'success')
-                          });
+                        this._router.navigate([userType.toLowerCase()], {
+                          state: { data: patientDto } // Pass the 'res' object as 'data' in the state object
+                        }).then(r => {
+                          this.ToasterService.customToast(loginResponse.message ? loginResponse.message : " ", 'success')
                         });
-                    }
-                    else {
-                      this.isLoading = false;
-                      this.hasError = true;
-                      this.ToasterService.customToast(loginResponse.message ? loginResponse.message : " ", 'error');
-                    }
+                      });
                   }
 
                   else {
                     this.isLoading = false;
+                    this.hasError = true;
                     this.ToasterService.customToast(
                       loginResponse.message ? loginResponse.message : ' ',
                       'error'
