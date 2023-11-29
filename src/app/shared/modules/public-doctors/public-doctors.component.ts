3f8d5bd8a611637c5a98c7c4509ab03024bfe92c
shaikat.b <shaikat.b@coppanet.com>
2023-11-30 02:14:08 +0600

    ......

--- src/app/shared/modules/public-doctors/public-doctors.component.ts
+++ src/app/shared/modules/public-doctors/public-doctors.component.ts
@@ -6,7 +6,7 @@ import { DoctorProfileService } from './../../../proxy/services/doctor-profile.s
 import { Component, Input, OnInit } from '@angular/core';
 import { UserinfoStateService } from '../../services/states/userinfo-state.service';
 import { AuthService } from '../../services/auth.service';
-import { DoctorProfileDto, SpecialityDto, SpecializationDto } from 'src/app/proxy/dto-models';
+import { DoctorProfileDto, FilterModel, SpecialityDto, SpecializationDto } from 'src/app/proxy/dto-models';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CommonService } from '../../services/common.service';
 import { ConsultancyType } from 'src/app/proxy/enums';
@@ -20,31 +20,31 @@ import { Subscription, startWith } from 'rxjs';
 })
 export class PublicDoctorsComponent implements OnInit {
   doctorList: DoctorProfileDto[] = [];
-  dataLoading:boolean = true
-  filterForm!:FormGroup
-  consultancyType!:ListItem[]
-  @Input() from!:string
+  dataLoading: boolean = true
+  filterForm!: FormGroup
+  consultancyType!: ListItem[]
+  @Input() from!: string
   specialityList: SpecialityDto[] = [];
   subscriptions: Subscription[] = [];
   specializationList: any;
 
-  //filter: FilterModel = {
-  //  offset: 0,
-  //  limit: 0,
-  //  pageNo: 1,
-  //  pageSize: 10,
-  //  sortBy: 'name',
-  //  sortOrder: 'asc',
-  //  isDesc: false,
-  //};
+  filter: FilterModel = {
+    offset: 0,
+    limit: 0,
+    pageNo: 1,
+    pageSize: 10,
+    sortBy: 'name',
+    sortOrder: 'asc',
+    isDesc: false,
+  };
   constructor(
     private UserinfoStateService: UserinfoStateService,
     private NormalAuth: AuthService,
     private DoctorStateService: DoctorStateService,
-    private fb : FormBuilder,
-    private SpecialityService : SpecialityService,
-    private SpecializationService : SpecializationService
-  ) {}
+    private fb: FormBuilder,
+    private SpecialityService: SpecialityService,
+    private SpecializationService: SpecializationService
+  ) { }
 
   ngOnInit(): void {
     this.loadForm();
@@ -60,9 +60,52 @@ export class PublicDoctorsComponent implements OnInit {
 
     this.subscriptions.push(specialitySubscription);
 
+    //let id = this.NormalAuth.authInfo().id;
+    //if (id) {
+    //  this.UserinfoStateService.getUserPatientInfo(id, 'patient');
+
+    //  if (this.DoctorStateService.doctorsList.value.length <= 0) {
+    //    const doctorListSubscription = this.DoctorStateService.getAllDoctorList().subscribe(
+    //      (res) => {
+    //        this.doctorList = res;
+    //        this.dataLoading = false;
+    //      }
+    //    );
+
+    //    this.subscriptions.push(doctorListSubscription);
+    //  } else {
+    //    const doctorListSubscription = this.DoctorStateService.getDoctorListData().subscribe(
+    //      (res) => {
+    //        this.doctorList = res;
+    //        this.dataLoading = false;
+    //      }
+    //    );
+
+    //    this.subscriptions.push(doctorListSubscription);
+    //  }
+    //}
+
+    this.loadDoctorsList();
+
+    const selectedSpeciality$: any = this.filterForm
+      .get('specialty')
+      ?.valueChanges.pipe(startWith(this.filterForm.get('specialty')?.value));
+
+    selectedSpeciality$.subscribe((data: any) => {
+      if (data) {
+        this.getSpecializations(data)
+      }
+    })
+  }
+
+  loadDoctorsList() {
+
     let id = this.NormalAuth.authInfo().id;
     if (id) {
       this.UserinfoStateService.getUserPatientInfo(id, 'patient');
+
+      this.filter.limit = this.filter.pageSize;
+      this.filter.offset = (this.filter.pageNo - 1) * this.filter.pageSize;
 
       if (this.DoctorStateService.doctorsList.value.length <= 0) {
         const doctorListSubscription = this.DoctorStateService.getAllDoctorList().subscribe(
@@ -85,18 +128,9 @@ export class PublicDoctorsComponent implements OnInit {
       }
     }
 
-    const selectedSpeciality$: any = this.filterForm
-    .get('specialty')
-    ?.valueChanges.pipe(startWith(this.filterForm.get('specialty')?.value));
-
-    selectedSpeciality$.subscribe((data: any) => {
-      if (data) {
-          this.getSpecializations(data)
-      }
-    })
   }
 
-  loadForm(){
+  loadForm() {
     this.filterForm = this.fb.group({
       consultancyType: ['0'],
       specialty: [''],
@@ -105,9 +139,9 @@ export class PublicDoctorsComponent implements OnInit {
   }
 
 
-  getSpecializations(id:any){
+  getSpecializations(id: any) {
     const specialitySubscription = this.SpecializationService.getListBySpecialtyId(id).subscribe({
-      next: (res) => { 
+      next: (res) => {
         this.specializationList = res;
       },
       complete: () => {
@@ -115,10 +149,22 @@ export class PublicDoctorsComponent implements OnInit {
       }
     });
   }
-submit(){
-  console.log(this.filterForm.value);
-  
-}
+  submit() {
+    console.log(this.filterForm.value);
+
+  }
+
+  //pageChanged($event: any) {
+  //  this.filter.pageNo = $event;
+  //  this.loadDoctorsList();
+  //}
+
+  //pageSizeChanged($event: any) {
+  //  this.filter.pageNo = 1;
+  //  this.filter.pageSize = $event;
+  //  this.loadDoctorsList();
+  //}
+
   ngOnDestroy(): void {
     this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
