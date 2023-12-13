569c4145209b620516df86fa9321bc7a5f8391e5
Muntasir Udoy <muntasir.u@gmail.com>
2023-12-13 12:14:21 +0600

    add q params

--- src/app/features-modules/public/landing-page/components/live-doctors/live-doctors.component.ts
+++ src/app/features-modules/public/landing-page/components/live-doctors/live-doctors.component.ts
@@ -2,6 +2,7 @@
 import { DoctorStateService } from 'src/app/shared/services/states/doctors-states/doctor-state.service';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import KeenSlider from 'keen-slider'
+import { Router } from '@angular/router';
 // import "keen-slider/dist/keen-slider.min.css"
 
 
@@ -15,42 +16,42 @@ export class LiveDoctorsComponent {
   @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
   slider: any = null
   slides = [
-    {color: '#007bff' ,text: 'Slider 1'}, 
-    {color: '#6c757d' ,text: 'Slider 2'},
-    {color: '#17a2b8' ,text: 'Slider 3'},
-    {color: '#28a745' ,text: 'Slider 4'},
-    {color: '#dc3545' ,text: 'Slider 5'},
-    {color: '#ffc107' ,text: 'Slider 6'}
+    { color: '#007bff', text: 'Slider 1' },
+    { color: '#6c757d', text: 'Slider 2' },
+    { color: '#17a2b8', text: 'Slider 3' },
+    { color: '#28a745', text: 'Slider 4' },
+    { color: '#dc3545', text: 'Slider 5' },
+    { color: '#ffc107', text: 'Slider 6' }
   ]
 
-  
-    
 
 
-    ngAfterViewInit() {
-      if (this.sliderRef && this.sliderRef.nativeElement) {
-        this.slider = new KeenSlider(this.sliderRef.nativeElement, {
-          slides: {
-            perView: 4,
-            spacing: 15,
-          },
-        });
-      }
+
+
+  ngAfterViewInit() {
+    if (this.sliderRef && this.sliderRef.nativeElement) {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        slides: {
+          perView: 4,
+          spacing: 15,
+        },
+      });
     }
-    
-  
+  }
+
+
 
   // ngOnDestroy(){
   //   if(this.slider) this.slider.destroy()
   // }
-liveDoctorList:any
-  constructor(private DoctorStateService : DoctorStateService){
+  liveDoctorList: any
+  constructor(private DoctorStateService: DoctorStateService,private router : Router) {
 
     this.DoctorStateService.getCurrentlyOnlineDoctorList().subscribe({
-      next:(res)=>{
+      next: (res) => {
         this.liveDoctorList = res
       },
-      error:(err)=>{
+      error: (err) => {
         console.log(err);
       },
       complete() {
@@ -61,7 +62,17 @@ liveDoctorList:any
 
 
 
+  onClickSeeMore(value:string) {
 
+
+    if (value) {
+      this.router.navigate(['/search'], { queryParams: { type:value } });
+    } else {
+      // Handle the case where searchText is undefined or falsy.
+      // You might want to show an error message or take appropriate action.
+    }
+
+  }
 
 
 }
