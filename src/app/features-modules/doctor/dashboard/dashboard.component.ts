8575af54d29d3a4944102f7f8e63ed6105d9a411
Muntasir Udoy <muntasir.u@gmail.com>
2023-12-20 17:24:02 +0600

    done patient doctor dashboard

--- src/app/features-modules/doctor/dashboard/dashboard.component.ts
+++ src/app/features-modules/doctor/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { DashboardService } from './../../../proxy/services/dashboard.service';
 import { Component, OnInit } from '@angular/core';
+import { AppointmentDto } from 'src/app/proxy/dto-models';
 
 
 @Component({
@@ -6,30 +9,65 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit{
-  showWarning:boolean = false
+export class DashboardComponent implements OnInit {
+  showWarning: boolean = false
   details = [
     {
       title: 'Total appointments',
-      data: 100,
+      data: 0,
     },
     {
       title: 'Total Patient',
-      data: '300',
+      data: '',
     },
     {
       title: 'Loyalty Points',
-      data: '100',
+      data: '',
     },
     {
       title: 'Total Income',
-      data: '1000tk',
+      data: '',
     },
   ];
-
+  doctorId: any;
+  appointmentList: AppointmentDto[] = [];
+  constructor(private NormalAuth: AuthService, private DashboardService: DashboardService) { }
+  selectedValue = "Today"
   ngOnInit(): void {
-    setTimeout(()=>{
+    setTimeout(() => {
       this.showWarning = true
-    },1000)
+    }, 1000)
+    let authId = this.NormalAuth.authInfo()?.id;
+    this.doctorId = authId
+    this.getDashboardStatisticData(authId)
+    this.getDashboardAppointment(this.selectedValue)
+  }
+  getDashboardStatisticData(id: number) {
+    this.DashboardService.getDashboadDataForDoctor(id).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.details[0].data = Number(res.totalAppointment)
+        this.details[1].data = Number(res.totalPatient)
+        this.details[2].data = Number(res.doctorLoyaltypoints)
+        this.details[3].data = Number(res.totalFeeAmount) + ".00 TK"
+      }
+    })
+  }
+  getDashboardAppointment(value: string) {
+    this.DashboardService.getDashboardAppointmentListForDoctor(this.doctorId, value).subscribe({
+      next: (res) => {
+        this.appointmentList = res
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+  changeSelection(e: any) {
+    if (e) {
+      this.getDashboardAppointment(e.target.value)
+      return
+    }
   }
 }
