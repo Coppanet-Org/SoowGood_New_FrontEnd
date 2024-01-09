6f314471caedd5075d7dd5a92921284a0def1ebf
ShaikatCopnet <shaikat.b@coppanet.com>
2024-01-09 19:25:59 +0600

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,9 +8,9 @@ import { Environment } from '@abp/ng.core';
 //const apiUrl = 'https://192.168.1.105:4437';
 //const issuerUrl = 'https://192.168.1.105';
 
-const baseUrl = 'https://103.1025.253.240:4439';
-const apiUrl = 'https://103.1025.253.240:4437';
-const issuerUrl = 'https://103.1025.253.240';
+const baseUrl = 'https://103.125.253.240:4439';
+const apiUrl = 'https://103.125.253.240:4437';
+const issuerUrl = 'https://103.125.253.240';
 
 
 export const environment = {
