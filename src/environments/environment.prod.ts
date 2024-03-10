d3cb26a0164d432b8285e0e8f412f2ceccebf76d
ShaikatCopnet <shaikat.b@coppanet.com>
2024-03-10 18:06:16 +0600

    .....

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,7 @@
 import { Environment } from '@abp/ng.core';
 
-//const baseUrl = 'https://103.125.253.240:4439';
-//const apiUrl = 'https://103.125.253.240:4437';
-//const issuerUrl = 'https://103.125.253.240';
-
-const baseUrl = 'https://soowgoodbeta.com';
-const apiUrl = 'https://apisoowgoodbeta.com';
+const baseUrl = 'https://apibetasoowgood.com';
+const apiUrl = 'https://soowgoodbeta.com';
 const issuerUrl = 'https://authsoowgoodbeta.com';
 
 export const environment = {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 import { Environment } from '@abp/ng.core';
 
-//const baseUrl = 'https://103.125.253.240:4439';
-//const apiUrl = 'https://103.125.253.240:4437';
-//const issuerUrl = 'https://103.125.253.240';
-//'https://apisoowgoodbeta.com'; //
-const baseUrl = 'https://soowgoodbeta.com';
-const apiUrl = 'https://localhost:44339';//
-const issuerUrl = 'https://authsoowgoodbeta.com'; //'https://localhost:44380';//
+
+const baseUrl = 'https://apibetasoowgood.com';
+const apiUrl = 'https://soowgoodbeta.com';
+const issuerUrl = 'https://authsoowgoodbeta.com';
+
 
 export const environment = {
   production: false,
