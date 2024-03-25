a828d29257db8854bc4bbbd9acdda68b86e46fe6
ShaikatCopnet <shaikat.b@coppanet.com>
2024-03-25 15:55:06 +0600

    .....

--- dist/soowgood-v2/chunk-GK2R5F73.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-BYPGBJQR.js";import"./chunk-5G567QLT.js";export default a();
--- dist/soowgood-v2/chunk-QOS45KNY.js
+++
@@ -1,1 +0,0 @@
-import{S as n,Y as i,cc as a}from"./chunk-TXQBTV46.js";var c=(()=>{let t=class t{};t.\u0275fac=function(o){return new(o||t)},t.\u0275mod=i({type:t}),t.\u0275inj=n({imports:[a]});let e=t;return e})();export{c as a};
